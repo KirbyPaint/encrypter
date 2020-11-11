98d8a99c444cabe9d43feccd8591dfb1b5d3f825
KirbyPaint <porter.ashley13@gmail.com>
2020-11-10 18:58:21 -0800

    clean up code, add third function

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,25 @@
 function firstLastCaps(input) {
-  const firstCaps = input.charAt(0).toUpperCase() // converts first character to upper
-  const stringLength = input.length; // gets length of entire string
-  const lastChar = input.charAt(stringLength - 1); // gets last character in string
-  const lastCaps = lastChar.toUpperCase() // last character to upper
+  const firstCaps = input.charAt(0).toUpperCase()          // converts first character to upper
+  const stringLength = input.length;                       // gets length of entire string
+  const lastChar = input.charAt(stringLength - 1);         // gets last character in string
+  const lastCaps = lastChar.toUpperCase()                  // last character to upper
   const middleString = input.slice(1, (stringLength - 1)); // all characters in string EXCEPT first and last
-  const newString = firstCaps + lastCaps; // concatenates
+  const newString = firstCaps + lastCaps;                  // concatenates
   // optimize later?
   return newString;
 }
 
-function reverseFirstLastCaps(input) {
-  const firstCaps = input.charAt(0).toUpperCase() // converts first character to upper
-  const stringLength = input.length; // gets length of entire string
-  const lastChar = input.charAt(stringLength - 1); // gets last character in string
-  const lastCaps = lastChar.toUpperCase() // last character to upper
-  const middleString = input.slice(1, (stringLength - 1)); // all characters in string EXCEPT first and last
-  const newString = lastCaps + firstCaps; // concatenates
+function reverseTwoCharacters(input) {             // Function simply to reverse two characters
+  const firstCaps = input.charAt(0).toUpperCase(); // converts first character to upper
+  const lastCaps = input.charAt(1).toUpperCase();  // converts last character to upper
+  const newString = lastCaps + firstCaps;          // concatenates
 
   return newString;
+}
+
+function reverseFirstLastCaps(input) {         // Function to call the prev two functions
+  const toReverse = this.firstLastCaps(input); // Calls first function
+  return input + this.reverseTwoCharacters(toReverse); // Calls second function
 }
 
 $(document).ready(function() {
